95c4fd48872224d13a4af319536c8eef7d35ffdb
KarinaRV <karinaricioni39@gmail.com>
2021-11-20 01:29:19 -0300

    Blog Pessoal Deploy

---
+++ blogpessoal Deploy/blogpessoal/src/main/java/br/com/blogpessoal/service/UsuarioService.java
@@ -0,0 +1,88 @@
+package br.com.blogpessoal.service;
+
+import java.nio.charset.Charset;
+import java.util.Optional;
+
+import org.apache.commons.codec.binary.Base64;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import br.com.blogpessoal.model.UsuarioLogin;
+import br.com.blogpessoal.model.Usuario;
+import br.com.blogpessoal.repository.UsuarioRepository;
+
+@Service
+public class UsuarioService {
+
+	@Autowired
+	private UsuarioRepository repository;
+
+	public Optional<Usuario> CadastrarUsuario(Usuario usuario) {
+
+		if (repository.findByUsuario(usuario.getUsuario()).isPresent())
+			return Optional.empty();
+
+		usuario.setSenha(criptografarSenha(usuario.getSenha()));
+		return Optional.of(repository.save(usuario));
+
+	}
+
+	public Optional<Usuario> atualizarUsuario(Usuario usuario) {
+
+		if (repository.findById(usuario.getId()).isPresent()) {
+
+			Optional<Usuario> buscaUsuario = repository.findByUsuario(usuario.getUsuario());
+
+			if (buscaUsuario.isPresent()) {
+
+				if (buscaUsuario.get().getId() != usuario.getId())
+					return Optional.empty();
+			}
+			usuario.setSenha(criptografarSenha(usuario.getSenha()));
+			return Optional.of(repository.save(usuario));
+		}
+
+		return Optional.empty();
+	}
+
+	public Optional<UsuarioLogin> autenticarUsuario(Optional<UsuarioLogin> userLogin) {
+
+		Optional<Usuario> usuario = repository.findByUsuario(userLogin.get().getUsuario());
+
+		if (usuario.isPresent()) {
+			if (compararSenhas(userLogin.get().getSenha(), usuario.get().getSenha())) {
+
+				String token = gerarBasicToken(userLogin.get().getUsuario(), userLogin.get().getSenha());
+				userLogin.get().setId(usuario.get().getId());
+				userLogin.get().setNome(usuario.get().getNome());
+				userLogin.get().setSenha(usuario.get().getSenha());
+				userLogin.get().setToken(token);
+
+				return userLogin;
+			}
+		}
+
+		return Optional.empty();
+	}
+
+	private String criptografarSenha(String senha) {
+		
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		return encoder.encode(senha);
+	}
+
+	private boolean compararSenhas(String senhaDigitada, String senhaBanco) {
+		
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		return encoder.matches(senhaDigitada, senhaBanco);
+	}
+
+	private String gerarBasicToken(String email, String password) {
+		
+		String tokenBase = email + ":" + password;
+		byte[] tokenBase64 = Base64.encodeBase64(tokenBase.getBytes(Charset.forName("US-ASCII")));
+		return "Basic " + new String(tokenBase64);
+	}
+
+}
